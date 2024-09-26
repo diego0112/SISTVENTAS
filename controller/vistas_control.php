50645d125c2ead1dd5025d1616c6a29de0956d91
diego0112 <61715394+diego0112@users.noreply.github.com>
2024-09-26 12:57:47 -0500

    Update vistas_control.php
    ass

--- controller/vistas_control.php
+++ controller/vistas_control.php
@@ -5,11 +5,11 @@ require_once "./model/vistas_model.php";
 class vistasControlador extends vistaModelo {
 
     public function obtenerPlantillaControlador(){
-        return require_once "./view/plantilla.php";
+        return require_once "./views/plantilla.php";
     }
 
     public function obtenerVistaControlador(){
-        if($_GET['views']){
+        if(isset($_GET['views'])){
             $ruta = explode("/",$_GET['views']);
             $respuesta = vistaModelo::obtener_vista($ruta[0]);
         }else{
