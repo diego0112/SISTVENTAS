84aa92c5d6ae55651461561010098f86e361e1b6
diego0112 <diegoyalicocardenas@gmail.com>
2024-10-10 09:28:29 -0500

    a

--- model/vistas_model.php
+++ model/vistas_model.php
@@ -4,7 +4,7 @@ class vistaModelo{
 
     protected static function obtener_vista($vista){
     $palabras_permitidas =['login', 'usuario', 'producto', 'index', 'catalogo', 'contacto', 'detalledecarrito'
-     , 'detalleproducto', 'Mediosdepago', 'NuestraEmpresa', 'reclamaciones', 'TerminosyCondiciones'] ;
+     , 'DetalleProducto', 'Mediosdepago', 'NuestraEmpresa', 'reclamaciones', 'TerminosyCondiciones'] ;
         if (in_array($vista, $palabras_permitidas)){
             if(is_file("./views/".$vista.".php")){
                 $contenido = "./views/".$vista.".php";
