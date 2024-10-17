0b8cd16351d21733584393bafcd5477756b57e7c
AlexisXPL12 <valdiviatruciosalexisgabriel@gmail.com>
2024-10-17 11:48:55 -0500

    3

--- views/include/footer.php
+++ views/include/footer.php
@@ -1,66 +1,57 @@
-<footer class="bg-light text-dark py-4">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 mb-4">
-                <h4>WhatsApp:</h4>
+<footer>
+        <div class="footer-content">
+            <div class="footer-contact">
+                <h4>WhatsApp:</h4> 
                 <p>921310423 - 923545345</p>
-                <p>942385824 - 985752824</p>
+                <p>942385824 - 985752824</p> 
                 <p>912535523 - 924857834</p>
                 <p>Email: ventas@importecs.com</p>
                 <p>Dirección: <br> Av. San Martin N° 427-429</p>
                 <p>Soporte: 930794928</p>
                 <p>Dirección de Soporte:<br> Av. San Martin N° 427-429</p>
             </div>
-            
-            <div class="col-md-2 mb-4">
+            <div class="footer-social">
                 <h4>Síguenos</h4>
-                <a href="https://www.facebook.com/profile.php?id=100067636255042&locale=es_LA">
-                    <img src="./views/plantilla/img/iconofb.png" alt="Facebook" class="img-fluid">
-                    <p>Facebook</p>
-                </a>
-                <a href="https://www.instagram.com/andreedc01?igsh=NXFwYnc3ZGJlMjZl">
-                    <img src="./views/plantilla/img/intagram.png" alt="Instagram" class="img-fluid">
-                    <p>Instagram</p>
-                </a>
+                <a href="https://www.facebook.com/profile.php?id=100067636255042&locale=es_LA"><img src="./views/plantilla/img/iconofb.png" alt="Facebook"><p>Facebook</p></a>
+                <a href="https://www.instagram.com/andreedc01?igsh=NXFwYnc3ZGJlMjZl"><img src="./views/plantilla/img/intagram.png" alt="Instagram"><p>Instagram</p></a>
             </div>
-            
-            <div class="col-md-2 mb-4">
+
+            <div class="footer-links">
                 <h4>Información</h4>
-                <a id="info" href="<?php echo BASE_URL; ?>TerminosyCondiciones">Términos y Condiciones</a><br>
-                <a id="info" href="<?php echo BASE_URL; ?>NuestraEmpresa">Nuestra Empresa</a><br>
-                <a id="info" href="<?php echo BASE_URL; ?>reclamaciones">Libro de Reclamaciones</a><br>
+                <a id="info" href="<?php echo BASE_URL; ?>TerminosyCondiciones">Términos y Condiciones</a>
+                <a id="info" href="<?php echo BASE_URL; ?>NuestraEmpresa">Nuestra Empresa</a>
+                <a id="info" href="<?php echo BASE_URL; ?>reclamaciones">Libro de Reclamaciones</a>
                 <a id="info" href="<?php echo BASE_URL; ?>Mediosdepago">Medios de Pago</a>
             </div>
 
-            <div class="col-md-2 mb-4">
+            <div class="footer-policy">
                 <h4>Políticas y Condiciones</h4>
-                <a id="info" href="<?php echo BASE_URL; ?>contacto">Contáctanos</a><br>
-                <a id="info" href="#">Política de Privacidad de Datos Personales</a><br>
-                <a id="info" href="#">Política de Envío</a><br>
-                <a id="info" href="<?php echo BASE_URL; ?>TerminosyCondiciones">Términos y Condiciones</a>
+                <a  id="info" href="<?php echo BASE_URL; ?>contacto">Contactanos</a>
+                <a id="info" href="#">Política de Privacidad de Datos Personales</a>
+                <a id="info" href="#">Política de Envío</a>
+                <a id="info"  href="<?php echo BASE_URL; ?>TerminosyCondiciones">Términos y Condiciones</a>
             </div>
 
-            <div class="col-md-2 mb-4">
+            <div class="footer-newsletter">
                 <h4>Boletín de Anuncios</h4>
-                <input type="email" class="form-control mb-2" placeholder="Tu correo electrónico">
-                <button type="submit" class="btn btn-primary">Suscribirse</button>
-                <p class="small">Puedes darte de baja en cualquier momento. Para ello, consulte nuestra información de contacto en el aviso legal.</p>
+                <input type="email" placeholder="Tu correo electrónico">
+                <button type="submit">Suscribirse</button>
+                <p>Puedes darte de baja en cualquier momento. Para ello, consulte nuestra información de contacto en el aviso legal.</p>
             </div>
+
         </div>
 
-        <div class="row footer-bottom text-center py-3">
-            <div class="col-md-6">
-                <p>© 2024 IMPORTEC Solutions SAC. Todos los derechos reservados</p>
-            </div>
-            <div class="col-md-6">
-                <div class="footer-payments">
-                    <img src="./views/plantilla/img/visa.png" alt="Visa" class="img-fluid me-2">
-                    <img src="./views/plantilla/img/mastercard.png" alt="Mastercard" class="img-fluid me-2">
-                    <img src="./views/plantilla/img/americanexpress.png" alt="AmericanExpress" class="img-fluid me-2">
-                    <img src="./views/plantilla/img/bcp.png" alt="BCP" class="img-fluid me-2">
-                </div>
+        <div class="footer-bottom">
+            <p>© 2024 IMPORTEC Solutions SAC. Todos los derechos reservados</p>
+            <div class="footer-payments">
+                <img src="./views/plantilla/img/visa.png" alt="Visa">
+                <img src="./views/plantilla/img/mastercard.png" alt="Mastercard">
+                <img src="./views/plantilla/img/americanexpress.png" alt="AmericanExpress">
+                <img src="./views/plantilla/img/bcp.png" alt="BCP">
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
+</body>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> 
 
+</html>
