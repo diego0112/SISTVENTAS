2857ff42599b05b4aa0e6b22d1612e857416fe71
diego0112 <diegoyalicocardenas@gmail.com>
2024-11-13 23:29:48 -0500

    7

--- controller/vistas_control.php
+++ controller/vistas_control.php
@@ -3,11 +3,9 @@
 require_once "./model/vistas_model.php";
 
 class vistasControlador extends vistaModelo {
-
     public function obtenerPlantillaControlador(){
         return require_once "./views/plantilla.php";
     }
-
     public function obtenerVistaControlador(){
         if(isset($_GET['views'])){
             $ruta = explode("/",$_GET['views']);
@@ -18,5 +16,4 @@ class vistasControlador extends vistaModelo {
         return $respuesta;
     }
 }
-
 ?>
